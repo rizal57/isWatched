d5484a59d5b5e4a11244823dffc1951e3163a391
rizal57 <fahrurrzl5798@gmail.com>
2023-09-17 06:09:35 +0700

    [FIX] fix failed fetching when deploy

--- src/App.jsx
+++ src/App.jsx
@@ -102,7 +102,7 @@ export default function App() {
         setError('');
 
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+          `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
           { signal: controller.signal }
         );
         if (!res.ok)
--- src/components/DetailMovie.jsx
+++ src/components/DetailMovie.jsx
@@ -55,7 +55,7 @@ function DetailMovie({ selectedId, onClose, onAddWatched, watched }) {
       try {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
 
